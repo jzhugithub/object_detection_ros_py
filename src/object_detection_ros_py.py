7d4e7eed26c25f3f2a83542e4577331dc43df174
jzhugithub <mfx106825372@163.com>
2017-09-08 20:04:05 +0800

    fix video writer bug

--- src/object_detection_ros_py.py
+++ src/object_detection_ros_py.py
@@ -13,11 +13,12 @@ class DetectVideo(object):
     # parameters need to modify
     # node
     subscribed_topic = '/my_video'
-    # video
+    # video_output
     show_video_flag = True
-    save_video_flag = False
+    save_video_flag = True
     video_rate = 30.0
-    video_name = 'out.avi'
+    video_output_path = os.path.join(os.path.abspath('../video_output'), 'out.avi')
+    VIDEO_WINDOW_NAME = 'video_output'
     # detect
     # Create DetectImage class
     OBJECT_DETECTION_PATH = '/home/zj/program/models/object_detection'
@@ -31,16 +32,14 @@ class DetectVideo(object):
     # parameters do not need to modify
     # node
     # image_sub_ = rospy.Subscriber()
-    # video
-    VIDEO_WINDOW_NAME = ''
+    # video_output
     image_hight = -1
     image_width = -1
-    video = cv2.VideoWriter()
-    video_file_path = os.path.join(os.path.abspath('./video'), video_name)
+    video = 'VideoWriter'
     # frame
     frame_num = 1
-    src_3 = np.array([])
-    dst_3 = np.array([])
+    src = np.array([])
+    dst = np.array([])
     cvi = CvBridge()
     # detect
     di = 'DetectImage'
@@ -48,9 +47,8 @@ class DetectVideo(object):
     def __init__(self):
         # node
         self.image_sub_ = rospy.Subscriber(self.subscribed_topic, Image, self.image_callback, queue_size=1)
-        # video
+        # video_output
         if self.show_video_flag:
-            self.VIDEO_WINDOW_NAME = 'video'
             cv2.namedWindow(self.VIDEO_WINDOW_NAME)
         # detect
         self.di = DetectImage(self.PATH_TO_CKPT, self.PATH_TO_LABELS, self.NUM_CLASSES)
@@ -60,25 +58,25 @@ class DetectVideo(object):
 
     def image_callback(self, msg):
         try:
-            self.src_3 = self.cvi.imgmsg_to_cv2(msg, 'bgr8')
+            self.src = self.cvi.imgmsg_to_cv2(msg, 'bgr8')
         except CvBridgeError as e:
             print(e)
             return
         if self.frame_num == 1:
-            self.image_hight, self.image_width, channels = self.src_3.shape
-            self.video = cv2.VideoWriter(self.video_file_path, cv2.cv.CV_FOURCC('M', 'J', 'P', 'G'), self.video_rate,
+            self.image_hight, self.image_width, channels = self.src.shape
+            self.video = cv2.VideoWriter(self.video_output_path, cv2.cv.CV_FOURCC('M', 'J', 'P', 'G'), self.video_rate,
                                          (self.image_width, self.image_hight))
         self.frame_num += 1
 
         # detect
         # self.src_3 = cv2.resize(self.src_3,(160, 120))
-        self.dst_3 = self.di.run_detect(self.src_3)[0]
+        self.dst = self.di.run_detect(self.src)[0]
 
-        # save and show video
+        # save and show video_output
         if self.save_video_flag:
-            self.video.write(self.dst_3)
+            self.video.write(self.dst)
         if self.show_video_flag:
-            cv2.imshow(self.VIDEO_WINDOW_NAME, self.dst_3)
+            cv2.imshow(self.VIDEO_WINDOW_NAME, self.dst)
             cv2.waitKey(1)
 
 
